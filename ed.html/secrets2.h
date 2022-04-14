c45254da662fc6b5891ebc7f0b156da70f5660e1
YusufOMA <90691334+YusufOMA@users.noreply.github.com>
2022-04-14 14:08:23 +0200

    les5/6

---
+++ ed.html/secrets2.h
@@ -0,0 +1,6 @@
+#define WIFI_SSID "Medialab"
+#define WIFI_PASSWORD "Mediacollege2"
+#define SERVER_HOST "data.softwaredeveloper.amsterdam"
+#define SERVER_PORT 443
+#define DEVICE_KEY "a9b8d10a" // gebruik jouw eigen key
+#define DEVICE_SECRET "xUfaqafA" // gebruik jouw eigen secret
